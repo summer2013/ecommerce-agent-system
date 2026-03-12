0378762ed96514cb3e7c56a429650d18edeef17c
summer <shiny_mi@163.com>
2026-03-12 16:16:27 +0800

    feat: Add redirect from root URL to admin interface in Django URL configuration.

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# config/urls.py
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
     path('admin/', admin.site.urls),
 ]
